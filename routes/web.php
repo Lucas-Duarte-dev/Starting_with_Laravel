aad9ae0a7f0cfe877d4e6f21571c296bd195d1cf
Lucas-Duarte-dev <lucasduarteb2000@hotmail.com>
2021-08-05 21:25:15 -0300

    Created routes from category and home

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Site\CategoryController;
+use App\Http\Controllers\Site\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::namespace('Site')->group(function () {
+    Route::get('/', [HomeController::class, '__invoke']);
+
+    Route::get('produtos', [CategoryController::class, 'index']);
+    Route::get('produtos/{slug}', [CategoryController::class, 'show']);
 });
