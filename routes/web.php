d5eb258b8216020252a95a739a920faec7f26024
Lucas-Duarte-dev <lucasduarteb2000@hotmail.com>
2021-08-07 17:19:42 -0300

    Created routes from contact, blog and about

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\Site\BlogController;
 use App\Http\Controllers\Site\CategoryController;
+use App\Http\Controllers\Site\ContactController;
 use App\Http\Controllers\Site\HomeController;
 use Illuminate\Support\Facades\Route;
 
@@ -20,4 +22,11 @@ Route::namespace('Site')->group(function () {
 
     Route::get('produtos', [CategoryController::class, 'index']);
     Route::get('produtos/{slug}', [CategoryController::class, 'show']);
+
+    Route::get('blog', [BlogController::class, '__invoke']);
+
+    Route::view('sobre', 'site.about.index');
+
+    Route::get('contato', [ContactController::class, 'index']);
+    Route::post('contato', [ContactController::class, 'form']);
 });
